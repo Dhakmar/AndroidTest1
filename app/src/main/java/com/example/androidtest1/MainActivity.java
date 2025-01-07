be272b9432b22560b56f87c81185f957dda5a6d8
Dhakmar <dhakmar@gmail.com>
2025-01-07 23:10:57 +0200

    add some colors, fonts, few widgets on screen

--- app/src/main/java/com/example/androidtest1/MainActivity.java
+++ app/src/main/java/com/example/androidtest1/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.androidtest1;
 
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +12,8 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView textViewTimer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +25,30 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        textViewTimer = findViewById(R.id.textViewTimer);
+        findViewById(R.id.btn_start).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startTimer(v);
+            }
+        });
+
+
+    }
+
+    public void startTimer(View v) {
+        CountDownTimer countDownTimer = new CountDownTimer(50000, 50) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                textViewTimer.setText(String.valueOf(millisUntilFinished / 100));
+            }
+
+            @Override
+            public void onFinish() {
+
+            }
+        };
+        countDownTimer.start();
     }
 }
